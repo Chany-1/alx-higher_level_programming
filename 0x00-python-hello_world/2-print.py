99d3283b1259278558810403c7ac2641a1ebb25f
chany-1 <chanywang9@gmail.com>
2024-06-12 05:45:36 -0700

    python

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,6 +1,2 @@
-#!/bin/bash
-<<<<<<< HEAD
-print ("Programming is like building a multilingual puzzle\n")
-=======
-print("Programming is like building a multilingual puzzle")
->>>>>>> 88ea756b889122c3328f8314a49414287a13252d
+#!/usr/bin/python3
+print("\"Programming is like building a multilingual puzzle")
